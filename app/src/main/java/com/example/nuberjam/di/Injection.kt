e6fc46e667aeba9f9165caa2810c875d0c592330
Rama Tri Agung <ramatriagung91@gmail.com>
2023-07-19 10:29:21 +0700

    refactor(general) : MP-227 Change Async Flow to Coroutine Flow

--- app/src/main/java/com/example/nuberjam/di/Injection.kt
+++ app/src/main/java/com/example/nuberjam/di/Injection.kt
@@ -5,15 +5,13 @@ import com.example.nuberjam.data.Repository
 import com.example.nuberjam.data.source.local.service.DbConfig
 import com.example.nuberjam.data.source.preferences.AppPreferences
 import com.example.nuberjam.data.source.remote.service.ApiConfig
-import com.example.nuberjam.utils.AppExecutors
 
 object Injection {
     fun provideRepository(context: Context): Repository {
         val apiService = ApiConfig.getApiService()
         val dbDao = DbConfig.getInstance(context).dbDao()
         val appPreferences = AppPreferences.getInstance(context)
-        val appExecutors = AppExecutors()
-        return Repository.getInstance(apiService, dbDao, appPreferences, appExecutors)
+        return Repository.getInstance(apiService, dbDao, appPreferences)
     }
 
 }
