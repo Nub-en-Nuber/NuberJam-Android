fe27e97c0556d2fa5b67fb4892859be05444c14e
Anjar Harimurti <acool90@yahoo.co.id>
2023-07-29 11:56:21 +0700

    feat(general): MP-240 Configure MainActivity

--- app/src/main/java/com/example/nuberjam/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/MainActivity.kt
@@ -2,12 +2,27 @@ package com.example.nuberjam.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.nuberjam.R
+import com.example.nuberjam.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navView: BottomNavigationView = binding.navView
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
     }
 }
