dc2b00f598f055c5c27e3678f77d4d0eb32abd22
Rama Tri Agung <ramatriagung91@gmail.com>
2023-07-30 08:13:59 +0700

    feat(account) : MP-246 Create Logic for Username Case

--- app/src/main/java/com/example/nuberjam/data/Repository.kt
+++ app/src/main/java/com/example/nuberjam/data/Repository.kt
@@ -1,7 +1,11 @@
 package com.example.nuberjam.data
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
 import com.example.nuberjam.data.source.local.service.DbDao
 import com.example.nuberjam.data.source.preferences.AppPreferences
+import com.example.nuberjam.data.source.remote.service.ApiConfig
 import com.example.nuberjam.data.source.remote.service.ApiService
 
 class Repository private constructor(
@@ -9,6 +13,19 @@ class Repository private constructor(
     private val dbDao: DbDao,
     private val appPreferences: AppPreferences
 ) {
+
+    fun checkUsernameExist(username: String): LiveData<Result<Boolean>> = liveData {
+        emit(Result.Loading)
+        try {
+            val response = apiService.readAccountWithUsername(username)
+            val status = response.status
+            if (status == ApiConfig.SUCCESS_CODE) emit(Result.Success(true))
+            else emit(Result.Success(false))
+        } catch (e: Exception) {
+            Log.e("Repository", "checkUsernameExist: ${e.message.toString()} ")
+            emit(Result.Error(e.message.toString()))
+        }
+    }
 
     companion object {
         @Volatile
