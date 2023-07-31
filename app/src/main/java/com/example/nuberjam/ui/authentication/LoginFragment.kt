5bfe821c58a902ffbc630ead698edd16f6b8c030
Anjar Harimurti <acool90@yahoo.co.id>
2023-07-31 14:34:06 +0700

    feat(account): MP-275 Connect Auth Navigation and Main Navigation

--- app/src/main/java/com/example/nuberjam/ui/authentication/LoginFragment.kt
+++ app/src/main/java/com/example/nuberjam/ui/authentication/LoginFragment.kt
@@ -5,19 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.nuberjam.R
+import com.example.nuberjam.databinding.FragmentHomeBinding
+import com.example.nuberjam.databinding.FragmentLoginBinding
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(), View.OnClickListener {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        binding.btnLogin.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.btn_login -> {
+                v.findNavController().navigate(R.id.action_navigation_login_to_mainActivity)
+                requireActivity().finish()
+            }
+        }
+    }
 }
