2d8bec3f416b4f73b9dcbb66c79fcbe945448a38
Anjar Harimurti <acool90@yahoo.co.id>
2023-08-31 14:55:14 +0700

    feat(album + music + music-artist): Create Album, Music, & Artist Model & Data Mapper

--- app/src/main/java/com/example/nuberjam/data/model/Music.kt
+++ app/src/main/java/com/example/nuberjam/data/model/Music.kt
@@ -1,3 +1,15 @@
 package com.example.nuberjam.data.model
 
-class Music
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Music(
+    val playlistId: Int?,
+    val id : Int?,
+    val name : String?,
+    val duration: String?,
+    val file : String?,
+    val artist: List<Artist>?,
+    val isFavorite: Boolean?
+): Parcelable
--- app/src/main/java/com/example/nuberjam/utils/Mapping.kt
+++ app/src/main/java/com/example/nuberjam/utils/Mapping.kt
@@ -1,7 +1,13 @@
 package com.example.nuberjam.utils
 
 import com.example.nuberjam.data.model.Account
+import com.example.nuberjam.data.model.Album
+import com.example.nuberjam.data.model.Artist
+import com.example.nuberjam.data.model.Music
 import com.example.nuberjam.data.source.remote.response.AccountItem
+import com.example.nuberjam.data.source.remote.response.AlbumItem
+import com.example.nuberjam.data.source.remote.response.MusicArtistItem
+import com.example.nuberjam.data.source.remote.response.MusicItem
 
 object Mapping {
     fun accountItemToAccount(data: AccountItem): Account = Account(
@@ -12,4 +18,34 @@ object Mapping {
         password = data.accountPassword,
         photo = data.accountPhoto
     )
+
+    fun albumItemToAlbum(data: AlbumItem) : Album = Album(
+        id = data.albumId,
+        name = data.albumName,
+        photo = data.albumPhoto,
+        music = if (data.music.isEmpty()) null else musicItemToMusic(data.music)
+    )
+
+    fun musicItemToMusic(data: List<MusicItem>) : List<Music> {
+        return data.map { musicData ->
+            Music(
+                playlistId = musicData.playlistDetailId,
+                id = musicData.musicId,
+                name = musicData.musicName,
+                duration = musicData.musicDuration,
+                file = musicData.musicFile,
+                artist = artistItemToArtist(musicData.musicArtist),
+                isFavorite = musicData.musicIsFavorite
+            )
+        }
+    }
+
+    fun artistItemToArtist(data: List<MusicArtistItem>) : List<Artist> {
+        return data.map { artistData ->
+            Artist(
+                id = artistData.artistId,
+                name = artistData.artistName
+            )
+        }
+    }
 }
