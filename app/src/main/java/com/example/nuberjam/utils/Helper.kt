12ad88b91927734e53bfee912edcf65f94e264e0
Anjar Harimurti <acool90@yahoo.co.id>
2023-09-01 06:51:59 +0700

    feat(music): MP-315 Create Music Adapter

--- app/src/main/java/com/example/nuberjam/utils/Helper.kt
+++ app/src/main/java/com/example/nuberjam/utils/Helper.kt
@@ -1,3 +1,20 @@
 package com.example.nuberjam.utils
 
-object Helper
+import com.example.nuberjam.data.model.Artist
+
+object Helper {
+
+    fun concatenateArtist(artists: List<Artist>) = artists.joinToString(", ")
+
+    fun displayDuration(duration: Int): String {
+        val hour = duration / 3600
+        val minute = (duration%3600) / 60
+        val second = duration % 60
+        val fixSecond = if (second < 10) "0${second}" else second
+
+        var durations = "${minute}:${fixSecond}"
+        if (hour > 0)
+            durations = "$hour:$durations"
+        return durations
+    }
+}
