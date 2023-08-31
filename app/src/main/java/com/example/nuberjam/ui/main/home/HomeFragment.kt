7c759e94a1ee9e70528948598850ee3af1c29a4f
Allyandaru <all4yandaru@gmail.com>
2023-08-31 19:30:39 +0700

    feat(general): MP-303 Wiring AppBar

--- app/src/main/java/com/example/nuberjam/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/home/HomeFragment.kt
@@ -2,10 +2,21 @@ package com.example.nuberjam.ui.main.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.nuberjam.R
 import com.example.nuberjam.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,4 +36,17 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        val searchButton = binding.homeAppbar.btnSearch
+
+        searchButton.setOnClickListener{
+            // TODO: navigate to search
+            Toast.makeText(requireActivity(), "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
