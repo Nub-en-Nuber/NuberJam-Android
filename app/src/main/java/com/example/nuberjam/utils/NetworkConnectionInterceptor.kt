02bfc6231f8053e840e289286014c3e224425eb6
Rama Tri Agung <ramatriagungwork@gmail.com>
2023-09-06 15:18:48 +0700

    feat(general) : MP-351 General Snackbar Error: Interceptor Class

---
+++ app/src/main/java/com/example/nuberjam/utils/NetworkConnectionInterceptor.kt
@@ -0,0 +1,39 @@
+package com.example.nuberjam.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import okhttp3.Interceptor
+import okhttp3.Interceptor.*
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class NetworkConnectionInterceptor(private val mContext: Context) : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Chain): Response {
+        if (!isConnected) {
+            throw NoConnectivityException()
+        }
+        val builder: Request.Builder = chain.request().newBuilder()
+        return chain.proceed(builder.build())
+    }
+
+    private val isConnected: Boolean
+        get() {
+            val connectivityManager =
+                mContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val capabilities =
+                connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+
+            if (capabilities != null) {
+                return when {
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                    capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                    else -> false
+                }
+            }
+            return false
+        }
+}
