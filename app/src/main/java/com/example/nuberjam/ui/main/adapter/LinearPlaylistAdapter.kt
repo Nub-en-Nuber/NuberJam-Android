ee3082a739744f5c0872137b461121b4dc9aafcd
Anjar Harimurti <acool90@yahoo.co.id>
2023-09-23 22:49:39 +0700

    feat(playlist): MP-367 Clean Code

--- app/src/main/java/com/example/nuberjam/ui/main/adapter/LinearPlaylistAdapter.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/adapter/LinearPlaylistAdapter.kt
@@ -3,19 +3,20 @@ package com.example.nuberjam.ui.main.adapter
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.nuberjam.R
-import com.example.nuberjam.data.model.Album
 import com.example.nuberjam.data.model.Playlist
 import com.example.nuberjam.databinding.LinearPlaylistItemBinding
 
-class LinearPlaylistAdapter : ListAdapter<Playlist, LinearPlaylistAdapter.ViewHolder>(DIFF_CALLBACK) {
+class LinearPlaylistAdapter :
+    ListAdapter<Playlist, LinearPlaylistAdapter.ViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = LinearPlaylistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            LinearPlaylistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -24,7 +25,8 @@ class LinearPlaylistAdapter : ListAdapter<Playlist, LinearPlaylistAdapter.ViewHo
         holder.bind(playlistItem)
     }
 
-    class ViewHolder(private var binding: LinearPlaylistItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private var binding: LinearPlaylistItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(playlistItem: Playlist) {
             binding.apply {
                 Glide.with(itemView.context).load(playlistItem.photo).into(ivPlaylistImage)
@@ -33,22 +35,22 @@ class LinearPlaylistAdapter : ListAdapter<Playlist, LinearPlaylistAdapter.ViewHo
             }
         }
     }
-
-
+    
     companion object {
-        val DIFF_CALLBACK: DiffUtil.ItemCallback<Playlist> = object : DiffUtil.ItemCallback<Playlist>() {
-            override fun areItemsTheSame(
-                oldItem: Playlist, newItem: Playlist
-            ): Boolean {
-                return oldItem.id == newItem.id
-            }
+        val DIFF_CALLBACK: DiffUtil.ItemCallback<Playlist> =
+            object : DiffUtil.ItemCallback<Playlist>() {
+                override fun areItemsTheSame(
+                    oldItem: Playlist, newItem: Playlist
+                ): Boolean {
+                    return oldItem.id == newItem.id
+                }
 
-            @SuppressLint("DiffUtilEquals")
-            override fun areContentsTheSame(
-                oldItem: Playlist, newItem: Playlist
-            ): Boolean {
-                return oldItem == newItem
+                @SuppressLint("DiffUtilEquals")
+                override fun areContentsTheSame(
+                    oldItem: Playlist, newItem: Playlist
+                ): Boolean {
+                    return oldItem == newItem
+                }
             }
-        }
     }
 }
