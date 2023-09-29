c6db2def71c676e332996df133cd749f89d0e511
Allyandaru <all4yandaru@gmail.com>
2023-09-29 19:42:00 +0700

    feat(playlist): MP-369 Show Snackbar Error and Shimmer Loading

--- app/src/main/java/com/example/nuberjam/ui/main/library/LibraryViewModel.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/library/LibraryViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.nuberjam.ui.main.library
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.nuberjam.data.Repository
+import com.example.nuberjam.ui.customview.CustomSnackbar
+import com.example.nuberjam.utils.Event
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -9,6 +13,13 @@ import javax.inject.Inject
 class LibraryViewModel @Inject constructor(
     private val repository: Repository
 ) : ViewModel() {
+
+    private val _snackbarState = MutableLiveData<Event<CustomSnackbar.SnackbarState>>()
+    val snackbarState: LiveData<Event<CustomSnackbar.SnackbarState>> = _snackbarState
+
+    fun setSnackbar(message: String, state: Int, length: Int = CustomSnackbar.LENGTH_LONG) {
+        _snackbarState.value = Event(CustomSnackbar.SnackbarState(message, state, length))
+    }
 
     fun getAccountState() = repository.getAccountState()
 
