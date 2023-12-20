656cde4eee5a13644caf89f107541377dfcfd824
RTAgung <ramatriagungwork@gmail.com>
2023-12-20 14:14:31 +0700

    feat(favorite): 860t23d1v Wiring API data to UI

--- app/src/main/java/com/example/nuberjam/ui/main/library/detail/DetailLibraryFragment.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/library/detail/DetailLibraryFragment.kt
@@ -1,21 +1,125 @@
 package com.example.nuberjam.ui.main.library.detail
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nuberjam.R
+import com.example.nuberjam.data.Result
+import com.example.nuberjam.databinding.FragmentDetailLibraryBinding
+import com.example.nuberjam.ui.customview.CustomSnackbar
+import com.example.nuberjam.ui.main.adapter.MusicAdapter
+import com.example.nuberjam.utils.Helper
+import com.example.nuberjam.utils.extensions.collectLifecycleFlow
+import com.example.nuberjam.utils.extensions.showNuberJamDefaultState
+import com.example.nuberjam.utils.extensions.showNuberJamEmptyState
+import com.example.nuberjam.utils.extensions.showNuberJamLoadingState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class DetailLibraryFragment : Fragment() {
+
+    private var _binding: FragmentDetailLibraryBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: DetailLibraryViewModel by viewModels()
+
+    private lateinit var adapter: MusicAdapter
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail_library, container, false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailLibraryBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupAppbar()
+        setupRecyclerView()
+        showSnackbarObserve()
+        observeFavoriteData()
+    }
+
+    private fun setupRecyclerView() {
+        adapter = MusicAdapter(object : MusicAdapter.MusicAdapterCallback {
+            override fun onItemClick(musicId: Int) {
+//                TODO("Not yet implemented")
+            }
+        })
+        binding.rvMusicList.apply {
+            adapter = adapter
+            layoutManager = LinearLayoutManager(requireActivity())
+        }
+    }
+
+    private fun observeFavoriteData() {
+        viewLifecycleOwner.collectLifecycleFlow(viewModel.favoriteState) { result ->
+            if (result != null) {
+                when (result) {
+                    is Result.Loading -> binding.msvPlaylistOuter.showNuberJamLoadingState()
+                    is Result.Success -> {
+                        binding.msvPlaylistOuter.showNuberJamDefaultState()
+                        val data = result.data
+                        binding.imvCover.tvLibraryTitle.text = getString(R.string.liked_song)
+                        binding.imvCover.tvLibraryType.text =
+                            getString(R.string.total_song, data.size)
+                        binding.imvCover.ivGridImage.setImageResource(R.drawable.favorite_pic)
+                        if (data.isEmpty()) {
+                            binding.msvPlaylistInner.showNuberJamEmptyState(
+                                emptyMessage = getString(R.string.data_not_available)
+                            )
+                        } else {
+                            adapter.submitList(data)
+                        }
+                    }
+
+                    is Result.Error -> {
+                        binding.msvPlaylistInner.showNuberJamEmptyState(
+                            emptyMessage = getString(R.string.data_not_available)
+                        )
+                        viewModel.setSnackbar(
+                            Helper.getApiErrorMessage(requireActivity(), result.errorCode),
+                            CustomSnackbar.STATE_ERROR
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupAppbar() {
+        val toolbar: Toolbar = binding.appbar.toolbar
+        toolbar.navigationIcon = ContextCompat.getDrawable(
+            requireContext(), R.drawable.ic_back_gray
+        )
+        toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+        binding.appbar.tvLibraryAppbar.text = getString(R.string.liked_song)
+        binding.appbar.btnSearch.setOnClickListener {
+            // TODO: navigate to search
+            Toast.makeText(requireActivity(), "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun showSnackbarObserve() {
+        viewModel.snackbarState.observe(viewLifecycleOwner) { event ->
+            event.getContentIfNotHandled()?.let { snackbarState ->
+                val customSnackbar =
+                    CustomSnackbar.build(layoutInflater, binding.root, snackbarState.length)
+                customSnackbar.setMessage(snackbarState.message)
+                customSnackbar.setState(snackbarState.state)
+                customSnackbar.show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/nuberjam/ui/main/library/detail/DetailLibraryViewModel.kt
+++ app/src/main/java/com/example/nuberjam/ui/main/library/detail/DetailLibraryViewModel.kt
@@ -1,11 +1,44 @@
 package com.example.nuberjam.ui.main.library.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.nuberjam.data.Repository
+import com.example.nuberjam.data.Result
+import com.example.nuberjam.data.model.Music
+import com.example.nuberjam.ui.customview.CustomSnackbar
+import com.example.nuberjam.utils.Event
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class DetailLibraryViewModel @Inject constructor(
     private val repository: Repository
 ) : ViewModel() {
 
+    private val _favoriteState = MutableStateFlow<Result<List<Music>>?>(null)
+    val favoriteState = _favoriteState.asStateFlow()
+
+    private val _snackbarState = MutableLiveData<Event<CustomSnackbar.SnackbarState>>()
+    val snackbarState: LiveData<Event<CustomSnackbar.SnackbarState>> = _snackbarState
+
+    init {
+        getFavoriteData()
+    }
+
+    fun setSnackbar(message: String, state: Int, length: Int = CustomSnackbar.LENGTH_LONG) {
+        _snackbarState.value = Event(CustomSnackbar.SnackbarState(message, state, length))
+    }
+
+    private fun getFavoriteData() {
+        viewModelScope.launch {
+            repository.readAllFavorite().collect {
+                _favoriteState.value = it
+            }
+        }
+    }
 }
